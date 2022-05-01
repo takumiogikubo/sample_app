a41b269a2c21fa58e3114eb182497bc200f48119
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-01 14:20:38 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get '/top'=> 'homes#top'
+  resources:lists
+  # get 'lists/new'
+  # post 'lists'=>'lists#create'
+  # get 'lists'=>'lists#index'
+  # get 'lists/:id'=>'lists#show',as: 'list'
+  # patch 'lists/:id'=>'lists#update',as:'update_list'
+  
+  # get    'lists/:id/edit'=>'lists#edit'   ,as:'edit_list'
+  # delete 'lists/:id'     =>'lists#destroy',as:'destroy_list'
+end
